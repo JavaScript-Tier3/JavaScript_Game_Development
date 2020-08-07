9da187a7df2e24be1b82db5aac08a1a1e03cab98
Ana Carrillo <acarrillo9724@gmail.com>
2020-08-06 23:57:49 -0400

    review let, const, and var vaiables

---
+++ variables.js
@@ -0,0 +1,17 @@
+/*
+What are variables?
+
+A way to keep track of values and data in our code.
+var name = value;
+
+*/
+var currentHealth = 100;
+currentHealth = 50;
+
+var isGameOver = false;
+isGameOver = true;
+
+//const, let, var
+const maxItem = 5; // this variable cannot be change
+var currentPrice = 6.25; // this variable can be change
+let scopeOnly = 8; // this can be changed or inovoke only if its inside the scope
