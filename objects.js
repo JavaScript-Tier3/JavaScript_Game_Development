eb9d7b6ed429b2bcdf86f5d87b216ca6adc30d78
Ana Carrillo <acarrillo9724@gmail.com>
2020-09-12 13:57:33 -0400

    defining objects

---
+++ objects.js
@@ -0,0 +1,8 @@
+// what are objects
+/* 
+Almost everything is an object except for primative values
+(booleans, numbers, strings, null, undefined)
+Typically we refer to objects as collections of name:value pairs
+Similar to dictionaries or hash maps in other languages
+can hold values or functions
+*/
