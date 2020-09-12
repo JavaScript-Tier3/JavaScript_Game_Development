d5971ddd59df04de570df941c31f2ef7fe4aa566
Ana Carrillo <acarrillo9724@gmail.com>
2020-09-12 14:16:25 -0400

    objects getters and setters

--- objects.js
+++ objects.js
@@ -9,20 +9,33 @@ can hold values or functions
 
 var gameCharacter = {
     name: "Ana",
+    class: "Human",
+    health: 100,
+    get tittle() { //getters
+        return this.name + "the" + this.class;
+    },
+    set maxHealth(h) {
+        this.health = h;
+    },
     xPos: 0,
     items: ["Knife", "Food"],
     move: function(x) {
         this.xPos += x;
     }
 };
+// GETTERS AND SETTERS
 
+
+
+/*
 var name = gameCharacter.name; //accessing values
 gameCharacter.items = ["axe", "bread"]; //changing values
 gameCharacter.moves(5);
 var x = gameCharacter.xPos;
 
 gameCharacter.yPos = 0;
-gameCharacter.move = function(x,y) {
+gameCharacter.move = function(x,y) { //COPYING THE REFERENCE
     this.xPos += x;
     this.yPos += y;
 }
+*/
