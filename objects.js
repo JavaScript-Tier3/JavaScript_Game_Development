d18f4535a266b53e4cdb259409412f345c4fea55
Ana Carrillo <acarrillo9724@gmail.com>
2020-09-12 14:05:48 -0400

    practiced writing objects

--- objects.js
+++ objects.js
@@ -6,3 +6,23 @@ Typically we refer to objects as collections of name:value pairs
 Similar to dictionaries or hash maps in other languages
 can hold values or functions
 */
+
+var gameCharacter = {
+    name: "Ana",
+    xPos: 0,
+    items: ["Knife", "Food"],
+    move: function(x) {
+        this.xPos += x;
+    }
+};
+
+var name = gameCharacter.name; //accessing values
+gameCharacter.items = ["axe", "bread"]; //changing values
+gameCharacter.moves(5);
+var x = gameCharacter.xPos;
+
+gameCharacter.yPos = 0;
+gameCharacter.move = function(x,y) {
+    this.xPos += x;
+    this.yPos += y;
+}
